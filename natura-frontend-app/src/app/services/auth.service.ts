8533350de80405a4cd32a5fcae7e52e7989b6dd7
devma12 <marion.delmas2@hotmail.fr>
2020-04-24 19:05:34 +0200

    share authentication between tabs

--- natura-frontend-app/src/app/services/auth.service.ts
+++ natura-frontend-app/src/app/services/auth.service.ts
@@ -14,10 +14,34 @@ export class AuthService {
     constructor(private router: Router,
         private userService: UserService) {
         this.user = new BehaviorSubject<User>(null);
+
+        const credentials = window.sessionStorage.getItem('JWT_TOKEN');
+        console.log(`token: ${credentials}`);
+
+        window.addEventListener('storage', (event) => {
+            
+            if (event.key === 'REQUESTING_TOKEN' && credentials) {
+                console.log('share token');
+              window.localStorage.setItem('SHARING_TOKEN', credentials);
+              window.localStorage.removeItem('SHARING_TOKEN');
+            }
+
+            if (event.key === 'SHARING_TOKEN' && !credentials) {
+              window.sessionStorage.setItem('JWT_TOKEN', event.newValue);
+              console.log('get requested token');
+              this.getAuthentication();
+            }
+          });
+
+          if (!credentials) {
+            console.log('request token');
+            window.localStorage.setItem('REQUESTING_TOKEN', '');
+            window.localStorage.removeItem('REQUESTING_TOKEN');
+          }
     }
 
     getToken(): string {
-        const token = sessionStorage.getItem('token');
+        const token = sessionStorage.getItem('JWT_TOKEN');
         console.log(`token: ${token}`);
         return token;
     }
@@ -52,7 +76,7 @@ export class AuthService {
     }
 
     private authenticate(value: User) {
-        sessionStorage.setItem('token', value.token);
+        sessionStorage.setItem('JWT_TOKEN', value.token);
         this.user.next(value);
         this.isAuth = true;
         this.router.navigate([this.redirectUrl]);
@@ -65,7 +89,7 @@ export class AuthService {
                 this.user.next(null);
                 this.isAuth = false;
                 this.router.navigate(['/login']);
-                sessionStorage.setItem('token', null);
+                sessionStorage.setItem('JWT_TOKEN', null);
             },
             error => {
                 console.error('Failed to logout');
