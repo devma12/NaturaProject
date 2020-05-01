98b231a031242e72b9afa3988f37fd9b9533a5ba
devma12 <marion.delmas2@hotmail.fr>
2020-05-01 11:27:35 +0200

    implement Spring Boot profiles for dev, test & prod environments

--- natura-server/src/test/java/com/natura/web/server/SqlTests.java
+++ natura-server/src/test/java/com/natura/web/server/SqlTests.java
@@ -1,25 +1,42 @@
 package com.natura.web.server;
 
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.config.MysqldConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql;
+import static com.wix.mysql.config.Charset.UTF8;
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.v5_6_23;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SqlTests {
 
-    //@Value("spring.datasource.url")
-    String url = "jdbc:mysql://localhost:3306/db_natura?useSSL=false&serverTimezone=UTC";
+    String url = "jdbc:mysql://localhost:3060/db_natura?useSSL=false&serverTimezone=UTC";
 
-    //@Value("spring.datasource.username")
     String user = "naturauser";
 
-    //@Value("spring.datasource.password")
     String password = "natura";
 
     @Test
     void testSqlConnection() throws Exception {
+
+        MysqldConfig config = aMysqldConfig(v5_6_23)
+                .withCharset(UTF8)
+                .withPort(3060)
+                .withUser(user, password)
+                .withTimeZone("Europe/Amsterdam")
+                .build();
+
+        EmbeddedMysql mysqld = anEmbeddedMysql(config)
+                .addSchema("db_natura")
+                .start();
+
         // Class.forName( "com.mysql.jdbc.Driver" ); // do this in init
         // // edit the jdbc url
         Connection conn = DriverManager.getConnection(url + "&user=" + user + "&password=" + password);
