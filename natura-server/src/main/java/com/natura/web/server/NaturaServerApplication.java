7745154233e952d5c7fa067b1907a48886f6e8f5
devma12 <63285332+devma12@users.noreply.github.com>
2022-03-12 17:35:20 +0100

    upgrade some dependencies version (#42)

--- natura-server/src/main/java/com/natura/web/server/NaturaServerApplication.java
+++ natura-server/src/main/java/com/natura/web/server/NaturaServerApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication {
+public class NaturaServerApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(NaturaServerApplication.class, args);
 	}
 
 }
