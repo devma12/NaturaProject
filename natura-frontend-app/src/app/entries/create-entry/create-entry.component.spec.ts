227999d9637078ee90f4888a7d3f4271968c4f7d
Marion <marion.delmas2@hotmail.fr>
2022-03-21 14:27:43 +0100

    migrate to angular and material version 13

--- natura-frontend-app/src/app/entries/create-entry/create-entry.component.spec.ts
+++ natura-frontend-app/src/app/entries/create-entry/create-entry.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CreateEntryComponent } from './create-entry.component';
 
@@ -6,7 +6,7 @@ describe('CreateEntryComponent', () => {
   let component: CreateEntryComponent;
   let fixture: ComponentFixture<CreateEntryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateEntryComponent ]
     })
