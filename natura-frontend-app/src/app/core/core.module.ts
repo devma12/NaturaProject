b0065bc1cfb11926529d614cb715146bfa109bb7
marion.delmas <marion.delmas-ext@gls-france.com>
2024-12-22 17:50:02 +0100

    upgrade to v18

--- natura-frontend-app/src/app/core/core.module.ts
+++ natura-frontend-app/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
